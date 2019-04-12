a1359c401124ff11b9c686c6cfc19741b5d2d484
jaredphil <phillips.jared@basco.com>
2019-04-12 18:38:13 -0500

    writes data in new file into 10 columns

--- add_comma.py
+++ add_comma.py
@@ -1,8 +1,17 @@
 # // Takes a file with 1 column of numbers and turns it into a new file with the numbers
 # // in just one row separated by commas
-data = open("workfile.txt").readlines()
+data = open("1000kHz BK 1024samples.txt").readlines()
 file = open("newfile.txt", "w")
+# for i in range (1, 10):
+#     print(i)
 for n, line in enumerate(data):
     data[n] = line.rstrip()
+    file.write(data[n])
+    file.write(',')
+    if n %10 == 9:
+        file.write('\n')
 
-file.write(','.join(data))
+        
+# file.write(','.join(data[line(0,10)]))
+
+# file.write(','.join(data))
---
+++ practice.py
@@ -0,0 +1,6 @@
+# hrs = input('How many hours did you work?')
+# pay = float(hrs) * 21
+# print("You made ",pay, " dollars")
+
+for i in range (0, 9):
+    print(i)
